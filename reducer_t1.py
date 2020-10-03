74361ea7be3df92690fc5c0314d407981382d3a3
Roshan Daivajna <roshan.daivajna@gmail.com>
2020-10-03 17:23:26 +0530

    Create first reducer

---
+++ reducer_t1.py
@@ -0,0 +1,19 @@
+import sys
+
+cur_node = None
+prev_node = None
+adj_list = []
+
+for line in sys.stdin:
+    cur_node, dest_node = line.strip().split('\t')
+    if cur_node == prev_node:
+        adj_list.append(dest_node)
+    else:
+        if prev_node:
+            print(prev_node, ",".join(adj_list), sep="\t")
+        prev_node = cur_node
+        adj_list.clear()
+        adj_list.append(dest_node)
+
+if cur_node == prev_node:
+    print(prev_node, ",".join(adj_list), sep="\t")
