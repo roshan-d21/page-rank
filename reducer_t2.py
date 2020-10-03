9da537e6502f2ab38bbdfdd90ab38aab780a230c
Roshan Daivajna <roshan.daivajna@gmail.com>
2020-10-03 21:13:11 +0530

    Update reducer2 to write to v1

--- reducer_t2.py
+++ reducer_t2.py
@@ -4,6 +4,8 @@ cur_node = None
 prev_node = None
 contrib_sum = 0
 
+v1 = open('v1', 'w')
+
 for line in sys.stdin:
     cur_node, contrib = line.strip().split(',')
 
@@ -13,11 +15,13 @@ for line in sys.stdin:
         if prev_node:
             new_pr = 0.15 + 0.85 * contrib_sum
             round(new_pr, 5)
-            print(prev_node, new_pr, sep=', ')
+            v1.write(prev_node + ', ' + str(new_pr))
         prev_node = cur_node
         contrib_sum = 0
 
 if cur_node == prev_node:
     new_pr = 0.15 + 0.85 * contrib_sum
     round(new_pr, 5)
-    print(prev_node, new_pr, sep=', ')
+    v1.write(prev_node + ', ' + str(new_pr))
+
+v1.close()
