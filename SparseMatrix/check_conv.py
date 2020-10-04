78ade06d375aed9978d551e6a17f62a9c5a75447
Roshan Daivajna <roshan.daivajna@gmail.com>
2020-10-04 23:15:29 +0530

    Update SparseMatrix scripts to support logging

--- SparseMatrix/check_conv.py
+++ SparseMatrix/check_conv.py
@@ -2,7 +2,8 @@ import shutil
 import os
 count = 0
 n = 0
-conv = 0.5  # this value will vary for different test cases in the backend
+conv = 0.0  # this value will vary for different test cases in the backend
+total_diff = 0.0
 
 
 def rewrite_pagerank():
@@ -13,17 +14,26 @@ def rewrite_pagerank():
     dest = shutil.copyfile(source, destination)
 
 
+def log(t, c):
+    with open('log', 'a') as logger:
+        logger.write('=' * 15 + '\n')
+        logger.write("Convergence:" + str(t/c) + '\n')
+        logger.write('=' * 15 + '\n')
+
+
 with open("v") as file1, open("v1") as file2:
     for line1, line2 in zip(file1, file2):
         count += 1
         old_pagerank = float(line1.split(",")[1])
         new_pagerank = float(line2.split(",")[1])
 
+        total_diff += abs(old_pagerank - new_pagerank)
         if(abs(old_pagerank-new_pagerank) < conv):
             n += 1
 
     if(n == count):
         print(0)
     else:
+        log(total_diff, count)
         rewrite_pagerank()
         print(1)
--- SparseMatrix/iterate-hadoop.sh
+++ SparseMatrix/iterate-hadoop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONVERGE=1
-rm v* log*
+rm v* log* m_out
 
 $HADOOP_HOME/bin/hadoop dfsadmin -safemode leave
 hdfs dfs -rm -r /output* 
@@ -9,19 +9,24 @@ $HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-*streamin
 -mapper "/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/mapper_t1.py" \
 -reducer "/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/reducer_t1.py '/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/v'"  \
 -input "/input_${1}" \
--output /output1 #has adjacency list
+-output /output1 #has sparse matrix
 
+ITERATIONS=0
 
 while [ "$CONVERGE" -ne 0 ]
 do
 	$HADOOP_HOME/bin/hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-*streaming*.jar \
-	-mapper "/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/mapper_t2.py '/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/v' " \
-	-reducer "/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/reducer_t2.py" \
-	-input /output1 \
+	-mapper "/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/mapper_t2.py '/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/v'" \
+	-reducer "/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/reducer_t2.py '/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/v1' '/home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/m_out'" \
+	-input /output1/part-00000 \
 	-output /output2
-	touch v1
-	hadoop fs -cat /output2/* > /home/roshan/Code/academics/sem5/BD/Assignment2_PageRank/SparseMatrix/v1
+	
+	ITERATIONS=$((ITERATIONS + 1))
+
 	CONVERGE=$(python3 check_conv.py >&1)
 	hdfs dfs -rm -r /output2
+
 	echo $CONVERGE
 done
+
+echo "Number of iterations = ${ITERATIONS}"
