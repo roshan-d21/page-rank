baaef1c18136751e411263af784efb85c2f74453
Roshan Daivajna <roshan.daivajna@gmail.com>
2020-10-04 23:19:26 +0530

    Update PageRank algorithm for SparseMatrix

--- SparseMatrix/mapper_t2.py
+++ SparseMatrix/mapper_t2.py
@@ -5,16 +5,13 @@ pageranks = {}  # to store pageranks of nodes in v, since it is guaranteed to fi
 
 with open(sys.argv[1], 'r') as v:
     for line in v:
-        node, rank = line.strip().split(',')
+        node, rank = line.strip().split(', ')
         pageranks[node] = rank
-
-m_out = open('m_out', 'w')
+# print(pageranks)
 
 for line in sys.stdin:
-    node, _, contrib = line.strip().split()
+    i, j, contrib = line.strip().split()
 
-    product = float(contrib) * float(pageranks[node])
+    product = float(contrib) * float(pageranks[j])
 
-    m_out.write(node + ', ' + str(product) + '\n')
-
-m_out.close()
+    print(i, product, sep=', ')
--- SparseMatrix/reducer_t2.py
+++ SparseMatrix/reducer_t2.py
@@ -5,17 +5,26 @@ cur_node = None
 prev_node = None
 new_pr = 0.0
 
-with open('m_out', 'r') as m_out, open('v1', 'w') as v1:
-    for line in m_out:
-        cur_node, contrib = line.strip().split(',')
+v1 = open(sys.argv[1], 'w')
+m_out = open(sys.argv[2], 'w')
 
-        if cur_node == prev_node:
-            new_pr += float(contrib)
-        else:
-            if prev_node:
-                v1.write(prev_node + ', ' + str(new_pr) + '\n')
-            prev_node = cur_node
-            new_pr = float(contrib)
+for line in sys.stdin:
+    cur_node, contrib = line.strip().split(', ')
 
     if cur_node == prev_node:
-        v1.write(prev_node + ', ' + str(new_pr) + '\n')
+        new_pr += float(contrib)
+        m_out.write(prev_node + ', ' + str(contrib) + '\n')
+    else:
+        if prev_node:
+            v1.write(prev_node + ', ' + str(new_pr) + '\n')
+            print(prev_node, new_pr, sep=', ')
+        prev_node = cur_node
+        new_pr = float(contrib)
+        m_out.write(prev_node + ', ' + str(contrib) + '\n')
+
+if cur_node == prev_node:
+    v1.write(prev_node + ', ' + str(new_pr) + '\n')
+    print(prev_node, new_pr, sep=', ')
+
+v1.close()
+m_out.close()
